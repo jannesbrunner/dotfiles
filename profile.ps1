700812c34f423594914d00b2bb08dc0b6775ed12
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2020-03-18 10:55:58 +0100

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -3,3 +3,6 @@ Import-Module oh-my-posh
 Import-Module DockerCompletion
 Set-Theme Paradox
 
+# Functions / Aliases
+function Update-Profile { Copy-Item "profile.ps1" -Destination "C:\Users\Jannes Brunner\Documents\WindowsPowerShell" }
+function Git-Config { param($param1, $param2, $param3); git --git-dir=$env:USERPROFILE/.cfg/ --work-tree=$env:USERPROFILE $param1 $param2 $param3 }
