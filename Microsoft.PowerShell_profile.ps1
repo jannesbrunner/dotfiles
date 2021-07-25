aaa03f837eb264303e14af7993454a511f4bf078
Jannes Brunner <jannes.brunner@oo-services.com>
2021-07-25 16:48:43 +0200

    Rename powershell profile name

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,12 @@
 Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module DockerCompletion
-Set-Theme Paradox
+Set-Theme Material
 
 # Functions / Aliases
 function Update-Profile { Copy-Item "profile.ps1" -Destination $env:userprofile\Documents\WindowsPowerShell }
-function Git-Config { param($param1, $param2, $param3); git --git-dir=$env:USERPROFILE/.cfg/ --work-tree=$env:USERPROFILE $param1 $param2 $param3 }
+function config { 
+    git --git-dir=$HOME/.cfg/ --work-tree=$HOME @args
+}
+
+Update-Profile # Always update the profile from $HOME to $env:userprofile\Documents\WindowsPowerShell
