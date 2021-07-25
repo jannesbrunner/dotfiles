4d9cfa248defbd1fca3aacb5a54616a5808349f7
Jannes Brunner <mail@jannesbrunner.de>
2021-07-25 17:10:30 +0200

    Switch to direct ps profile

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,9 +4,6 @@ Import-Module DockerCompletion
 Set-Theme Paradox
 
 # Functions / Aliases
-function Update-Profile { Copy-Item "$env:userprofile\Microsoft.PowerShell_profile.ps1" -Destination $env:userprofile\Documents\WindowsPowerShell }
 function config { 
     git --git-dir=$HOME/.cfg/ --work-tree=$HOME @args
 }
-
-Update-Profile # Always update the profile from $HOME to $env:userprofile\Documents\WindowsPowerShell
