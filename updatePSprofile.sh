364239017c8950e735741eea303003a05e2f51d0
Jannes Brunner <mail@jannesbrunner.de>
2022-07-31 11:48:57 +0200

    Remove script. Not needed anymore

--- updatePSprofile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp ~/Microsoft.PowerShell_profile.ps1 /mnt/c/users/Jay/Documents/WindowsPowerShell
-cp ~/Microsoft.PowerShell_profile.ps1 /mnt/c/users/JannesBrunner/Documents/WindowsPowerShell
